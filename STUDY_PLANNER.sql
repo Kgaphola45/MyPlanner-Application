1dbc55455a35c20b41072d4ae26f03bcd9197d01
Emmanuel kgaphola <It45ma@protonmail.ch>
2024-11-21 13:41:31 +0000

    Add student table to database

--- STUDY_PLANNER.sql
+++ STUDY_PLANNER.sql
@@ -31,3 +31,14 @@ self_study INT NOT NULL,
 hours_remain INT NOT NULL,
  
 );
+
+
+CREATE TABLE dbo.student (
+ 
+id INT IDENTITY(1,1) PRIMARY KEY,
+ 
+stdNumber VARCHAR(255) NOT NULL,
+ 
+password VARCHAR(255) NOT NULL
+);
+
